4ddf79d878bfd0e14b362896bba4d663573c3142
Oliveiralucass <gf.oliveiralucas@gmail.com>
2022-11-03 09:27:29 -0300

    Day 1

---
+++ day1/main.py
@@ -0,0 +1,14 @@
+print("Hello World!")
+print("Hello World!")
+print("Hello World!")
+
+print("Hello World!\nHello World!\nHello world!")
+
+print("Hello" + " " + "Lucas")
+
+print("Hello " + input("What is Your name?") + "!")
+
+
+name = "Jack"
+
+print(name)
